621030b7b9129bfbac6526c217cf340e5423fc76
cjfullerton <chris.j.fullerton@googlemail.com>
2022-06-23 11:28:36 +0100

    feat(remote): Completed RemoteClient class
    RemoteClient class now has all necessary functions, ready for testing
    and integration.

--- hpcflow_execution/RemoteClient.py
+++ hpcflow_execution/RemoteClient.py
@@ -1,6 +1,8 @@
+from multiprocessing import AuthenticationError
 from types import NoneType
 from paramiko import SSHClient, Transport, AutoAddPolicy
 import scp
+import logging
 
 class RemoteClient:
 
@@ -14,34 +16,112 @@ class RemoteClient:
         self.host = host
         self.user = user
         self.remote_path = remote_path
-        self.client_ssh = None
-        self.client_scp = NoneType
 
+        self.client_ssh = None
+        self.client_scp = None
+
+        self.logger = self.logger_setup()
+
+
+def logger_setup():
+        logger = logging.getLogger(__name__)
+
+        c_handler = logging.StreamHandler()
+        f_handler = logging.FileHandler('file.log')
+        c_handler.setLevel(logging.WARNING)
+        f_handler.setLevel(logging.ERROR)
+
+        c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+        f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        c_handler.setFormatter(c_format)
+        f_handler.setFormatter(f_format)
+
+        logger.addHandler(c_handler)
+        logger.addHandler(f_handler)
+
+        return logger
+
+
+@property
 def connection(self):
 
     self.client_ssh = ssh_connection(self.user, self.host)
     self.client_scp = scp_connection(self.client_ssh)
 
 
-def ssh_connection(uname, host):
+@connection.deleter
+def connection(self):
 
-    ssh = SSHClient()
-    ssh.set_missing_host_key_policy(AutoAddPolicy())
+    self.client_ssh.close()
+    self.client_scp.close()
 
-    ssh._transport = Transport(host)
-    ssh._transport.connect()
-    ssh._transport.auth_interactive_dumb(username=uname, handler=None)
 
-    return ssh
+def ssh_connection(self, uname, host):
 
-def scp_connection(ssh):
+    try:
+        ssh = SSHClient()
+        ssh.set_missing_host_key_policy(AutoAddPolicy())
+
+        ssh._transport = Transport(host)
+        ssh._transport.connect()
+        ssh._transport.auth_interactive_dumb(username=uname, handler=None)
+
+        return ssh
+    except AuthenticationException as e:
+        self.logger.error(
+            f"AuthenticationException occurred: {e}"
+            )
+    except Exception as e:
+        self.logger.error(
+            f"Unexpected error occurred while connecting to host: {e}"
+        )
+
+
+def scp_connection(self, ssh):
 
     scp = scp.SCPClient(ssh.get_transport())
 
     return scp
 
-def execute_command_ssh(self, command):
 
-    stdin, stdout, stderr = self.ssh_client.exec_command(command)
+def execute_commands(self, commands: list[str]):
 
-    return stdin, stdout, stderr
+    """
+    Execute multiple commands in succession.
+
+    :param List[str] dommands: List of unix commands as strings.
+    """
+
+    for cmd in commands:
+        stdin, stdout, stderr = self.connection.exec_command(cmd)
+        stdout.channel.recv_exit_status()
+        response = stdout.readlines()
+        for line in response:
+            self.logger.info(
+                f"INPUT: {cmd}\n \
+                OUTPUT: {line}"
+            )
+
+def bulk_upload(self, filepaths: list[str]):
+
+    try:
+        self.scp.put(
+            filepaths,
+            remote_path = self.remote_path,
+            recursive = True
+        )
+        self.logger.info(
+            f"Finished uploading {len(filepaths)} files to {self.remote_path} on {self.host}"
+        )
+    except SCPException as e:
+        self.logger.error(
+            f"SCPException during bulk upload: {e}"
+        )
+    except Exception as e:
+        self.logger.error(
+            f"Unexpected exception during bulk upload: {e}"
+        )
+
+def download_file(self, file: str):
+    """Download file from remote host."""
+    self.scp.get(file)
