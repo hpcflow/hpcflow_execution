e0db05af7f8afb61c292e4a8c59b5eaed95fecdb
cjfullerton <chris.j.fullerton@googlemail.com>
2022-07-28 11:54:55 +0100

    feat(Zarr): Added Zarr test script
    Added script to test zarr - creates, opens, manipulates, saves random
    numbers in zarr format.

--- hpcflow_execution/execution.py
+++ hpcflow_execution/execution.py
@@ -20,7 +20,7 @@ def run_elements(commands):
 
     print(f'Writing scripts and job submission files.')
     to_run = [
-        scriptgen.write_execution_files(task['command'], task_idx, \
+        scriptgen.write_execution_files(task['command'], task_idx,
             task['scheduler'], task['host_os'], workflow_path) 
         for task_idx, task in enumerate(commands[1:])
         ]
@@ -39,13 +39,17 @@ def run_elements(commands):
             
             if task['hostname'] not in remote_prep_done:
 
-                remote_clients[task['hostname']] = \
-                    RemoteClient.RemoteClient(task['hostname'], task['username'], task['basefolder'])
-                remote_folders[task['hostname']] = \
-                    create_remote_workflow_path(workflow_id, remote_clients[task['hostname']])
+                remote_clients[task['hostname']] = RemoteClient.RemoteClient(
+                    task['hostname'], task['username'], task['basefolder']
+                    )
+                remote_folders[task['hostname']] = create_remote_workflow_path(
+                    workflow_id, remote_clients[task['hostname']]
+                    )
                 remote_prep_done.add(task['hostname'])
 
-            remote_clients[task['hostname']].bulk_upload(remote_folders[task['hostname']], to_run[num][1:][0]) 
+            remote_clients[task['hostname']].bulk_upload(
+                remote_folders[task['hostname']], to_run[num][1:][0]
+                ) 
 
     # Now execute each task
     # NB For remote, queued tasks they are all getting submitted to the 
@@ -56,12 +60,16 @@ def run_elements(commands):
 
         if task['location'] == 'local':
             
-            command_info = subprocess.run(to_run[num][0], shell=True, cwd=workflow_path)
+            command_info = subprocess.run(
+                to_run[num][0], shell=True, cwd=workflow_path
+                )
             command_info.check_returncode()
 
         elif task['location'] == 'remote':
 
-            remote_clients[task['hostname']].execute_commands(remote_clients[task['hostname']].remote_path, [to_run[num][0]])
+            remote_clients[task['hostname']].execute_commands(
+                remote_clients[task['hostname']].remote_path, [to_run[num][0]]
+                )
 
     return to_run, scp_out
 
@@ -86,8 +94,9 @@ def create_remote_workflow_path(workflow_id, RemoteClient):
     print(f'Creating remote workflow folder at \
         {RemoteClient.host}:{remote_workflow_path}...')
 
-    RemoteClient.execute_commands(Path(RemoteClient.remote_path), \
-        [f'mkdir {remote_workflow_path}'])
+    RemoteClient.execute_commands(
+        Path(RemoteClient.remote_path), [f'mkdir {remote_workflow_path}']
+        )
 
     print(f'Complete\n')
 
