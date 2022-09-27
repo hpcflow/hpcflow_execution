a483b9bd7e26927f7ab5f63ba7e3de6e0d5cd3fe
cjfullerton <chris.j.fullerton@googlemail.com>
2022-09-27 16:18:52 +0100

    feat(cloud): Save entire output from dbx auth
    Worked out how to get refresh token and that there is more info
    (including token expiry time) included in return from
    auth_flow.finish(). Convert this all to a dict using  __dict__ and save
    in json format for future use.

--- hpcflow_execution/CloudDropbox.py
+++ hpcflow_execution/CloudDropbox.py
@@ -12,6 +12,40 @@ class CloudDropbox(CloudStorage):
         super().__init__(client_id)
         self.scopes = scopes
 
+    def get_authorization(self):
+
+        auth_flow = dropbox_api.DropboxOAuth2FlowNoRedirect(
+            self.client_id, use_pkce=True, scope=self.scopes, token_acces_type="offline"
+        )
+
+        authorization_url = auth_flow.start()
+
+        msg = dedent(
+            f"""
+        --------------------------- Connecting hpcflow to Dropbox ----------------------------
+            1. Go to this URL:
+            {authorization_url}
+            2. Click "Allow" (you might have to log in first).
+            3. Copy the authorization code below.
+        --------------------------------------------------------------------------------------
+        """
+        )
+        print(msg)
+
+        auth_code = input("Enter the authorization code here: ").strip()
+        oauth_result = auth_flow.finish(auth_code)
+
+        return oauth_result.__dict__
+
+    def save_authorization(auth_dict):
+
+        token_path = "config/dropbox_api_token.json"
+
+        token_string = json.dumps(auth_dict)
+
+        with open(token_path, "w") as file_out:
+            file_out.write(token_string)
+
     def generate_access_token(self):
 
         APP_KEY = self.client_id
