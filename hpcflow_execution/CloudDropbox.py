f8714004bf2207f0b7c8f707cf637af9d26fbcf5
cjfullerton <chris.j.fullerton@googlemail.com>
2022-10-05 11:02:15 +0100

    feat(cloud): Add more secure dropbox flow
    Add methods to de-authorize access keys and delete storage files. For
    security, we intend keys to be de-activated and files deleted after
    their use. Dropbox refresh keys have infinite lifetime.

--- hpcflow_execution/CloudDropbox.py
+++ hpcflow_execution/CloudDropbox.py
@@ -2,9 +2,13 @@ import json
 
 from textwrap import dedent
 from pathlib import Path
+from datetime import datetime
+import os
+import sys
 import dropbox as dropbox_api
 
 from hpcflow_execution.CloudStorage import CloudStorage
+from hpcflow_execution.JSONEncoders import DateTimeAwareEncoder
 
 
 class CloudDropbox(CloudStorage):
@@ -15,7 +19,10 @@ class CloudDropbox(CloudStorage):
     def get_authorization(self):
 
         auth_flow = dropbox_api.DropboxOAuth2FlowNoRedirect(
-            self.client_id, use_pkce=True, scope=self.scopes, token_acces_type="offline"
+            self.client_id,
+            use_pkce=True,
+            scope=self.scopes,
+            token_access_type="offline",
         )
 
         authorization_url = auth_flow.start()
@@ -37,63 +44,73 @@ class CloudDropbox(CloudStorage):
 
         return oauth_result.__dict__
 
-    def save_authorization(auth_dict):
+    def save_authorization(self, auth_dict):
 
         token_path = "config/dropbox_api_token.json"
 
-        token_string = json.dumps(auth_dict)
+        token_string = json.dumps(auth_dict, cls=DateTimeAwareEncoder)
 
         with open(token_path, "w") as file_out:
-            file_out.write(token_string)
+            json.dump(token_string, file_out, indent=4)
 
-    def generate_access_token(self):
-
-        APP_KEY = self.client_id
-        auth_flow = dropbox_api.DropboxOAuth2FlowNoRedirect(
-            APP_KEY, use_pkce=True, scope=self.scopes
-        )
-        authorize_url = auth_flow.start()
-
-        msg = dedent(
-            f"""
-        --------------------------- Connecting hpcflow to Dropbox ----------------------------
-            1. Go to this URL:
-            {authorize_url}
-            2. Click "Allow" (you might have to log in first).
-            3. Copy the authorization code below.
-        --------------------------------------------------------------------------------------
-        """
-        )
-        print(msg)
-
-        auth_code = input("Enter the authorization code here: ").strip()
-        oauth_result = auth_flow.finish(auth_code)
-        token = oauth_result.access_token
-
-        token_dict = {"AccessToken": token}
-
-        return token_dict
-
-    def save_token(self, token_dict):
+    def load_authorizaton(self):
 
         token_path = "config/dropbox_api_token.json"
 
-        token_string = json.dumps(token_dict)
+        try:
+            with open(token_path, "r") as file_in:
+                auth_string = file_in.read()
+        except FileNotFoundError:
+            print(f"File {token_path} not found!", file=sys.stderr)
+            return
+        except PermissionError:
+            print(f"Insufficient permission to read {token_path}!", file=sys.stderr)
+            return
+        except IsADirectoryError:
+            print(f"{token_path} is a directory!", file=sys.stderr)
+            return
 
-        with open(token_path, "w") as file_out:
-            file_out.write(token_string)
+        auth_dict = json.loads(auth_string)
 
-    def get_dropbox(self, token_dict):
+        return auth_dict
 
-        token = token_dict["AccessToken"]
+    def delete_authorization_file(self):
 
-        dbx = dropbox_api.Dropbox(token)
+        token_path = "config/dropbox_api_token.json"
+
+        os.remove(token_path)
+
+    def check_access_token_expiry(self, auth_dict):
+
+        token_expiry_time = auth_dict["expires_at"]
+
+        if datetime.now() > token_expiry_time:
+            return False
+        else:
+            return True
+
+    def get_dropbox_with_refresh(self, auth_dict):
+
+        token = auth_dict["access_token"]
+        token_expiry = auth_dict["expires_at"]
+        refresh_token = auth_dict["refresh_token"]
+
+        dbx = dropbox_api.Dropbox(
+            oauth2_access_token=token,
+            oauth2_refresh_token=refresh_token,
+            oauth2_access_token_expiration=token_expiry,
+            app_key=self.client_id,
+        )
 
         return dbx
 
+    def revoke_auth(self, dbx):
+
+        dbx.auth_token_revoke()
+
     def upload_file(self, dbx, local_path, local_file, dropbox_path):
 
-        local_file_path = Path(local_path) / local_file
+        local_file_path = Path("/" + local_path) / local_file
 
         mode = dropbox_api.files.WriteMode("overwrite")
 
---
+++ test_dropbox_flow.py
@@ -0,0 +1,37 @@
+import json
+
+from hpcflow_execution.CloudDropbox import CloudDropbox
+
+# Read in config details (app key, scopes)
+
+config_filename = "config/cloud_config.json"
+
+with open(config_filename, "r") as file_in:
+    config_string = file_in.read()
+    config_dict = json.loads(config_string)
+
+# Create dropbox interface object
+
+CloudDbx = CloudDropbox(
+    config_dict["dropbox"]["client_id"], config_dict["dropbox"]["scopes"]
+)
+
+# Get and save authorization keys
+
+auth_dict = CloudDbx.get_authorization()
+CloudDbx.save_authorization(auth_dict)
+
+# Create client and upload files
+
+dbx_client = CloudDbx.get_dropbox_with_refresh(auth_dict)
+CloudDbx.upload_file(
+    dbx_client,
+    "/Users/user/Documents/projects/hpcflow_execution",
+    "test.txt",
+    "/Apps/hpcflow_execution_test",
+)
+
+# Revoke access tokens, delete file
+
+CloudDbx.revoke_auth(dbx_client)
+CloudDbx.delete_authorization_file()
